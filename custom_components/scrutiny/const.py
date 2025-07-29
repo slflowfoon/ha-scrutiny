95e23c034f62c75f0e578a71198e99e7de327470
slflowfoon <94804320+slflowfoon@users.noreply.github.com>
2025-07-29 17:40:52 +0100

    Update const.py

--- custom_components/scrutiny/const.py
+++ custom_components/scrutiny/const.py
@@ -1,9 +1,7 @@
-"""Constants for the Scrutiny integration."""
 from datetime import timedelta
 
 DOMAIN = "scrutiny"
-DEFAULT_SCAN_INTERVAL = 15
+SCAN_INTERVAL = timedelta(minutes=15)
 
-# API Endpoints
 API_SUMMARY_ENDPOINT = "/api/summary"
 API_DETAILS_ENDPOINT_FORMAT = "/api/device/{wwn}/details"
