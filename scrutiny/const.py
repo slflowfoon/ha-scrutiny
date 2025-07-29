e7e04648c640d7bcbe5cdd9bf3d794f01d101d8a
slflowfoon <94804320+slflowfoon@users.noreply.github.com>
2025-07-29 15:15:42 +0100

    Delete scrutiny directory

--- scrutiny/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for the Scrutiny integration."""
-from datetime import timedelta
-
-DOMAIN = "scrutiny"
-SCAN_INTERVAL = timedelta(minutes=15)
-
-# API Endpoints
-API_SUMMARY_ENDPOINT = "/api/summary"
-API_DETAILS_ENDPOINT_FORMAT = "/api/device/{wwn}/details"
