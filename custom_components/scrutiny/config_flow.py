0cb98d7bafde91018580946319eabaacc90ea072
slflowfoon <94804320+slflowfoon@users.noreply.github.com>
2025-07-29 17:45:13 +0100

    Update config_flow.py

--- custom_components/scrutiny/config_flow.py
+++ custom_components/scrutiny/config_flow.py
@@ -1,54 +1,28 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
-from .const import DOMAIN, DEFAULT_SCAN_INTERVAL
+from .const import DOMAIN
 
+@config_entries.HANDLERS.register(DOMAIN)
 class ScrutinyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Scrutiny."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial setup step."""
         if user_input is not None:
-            await self.async_set_unique_id(user_input["base_url"].rstrip('/'))
+            data_to_store = {
+                "base_url": user_input["Scrutiny URL"].rstrip('/'),
+                "verify_ssl": user_input["Verify SSL Certificate"]
+            }
+
+            await self.async_set_unique_id(data_to_store["base_url"])
             self._abort_if_unique_id_configured()
             
-            return self.async_create_entry(title="Scrutiny", data=user_input)
+            return self.async_create_entry(title="Scrutiny", data=data_to_store)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required("Scrutiny URL"): str,
                 vol.Required("Verify SSL Certificate", default=True): bool,
-            })
-        )
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        """Get the options flow for this handler."""
-        return ScrutinyOptionsFlowHandler(config_entry)
-
-
-class ScrutinyOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle Scrutiny options."""
-
-    def __init__(self, config_entry):
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        """Manage the options."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema({
-                vol.Required(
-                    "scan_interval",
-                    default=self.config_entry.options.get("scan_interval", DEFAULT_SCAN_INTERVAL),
-                ): int,
             })
         )
